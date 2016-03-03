49dc036ad609ba62823aca6367f368bf377ac94d
Roberto <obi.serra@gmail.com>
2016-03-03 18:58:06 +0100

    decode fn implemented

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -6,7 +6,7 @@
 
 (defn letter-position
     [letter]
-    (- (- (int (to-char letter)) 97) 0))
+    (- (int (to-char letter)) 97))
 
 (defn fill-array [from to start]
   ""
@@ -20,13 +20,11 @@
     (map char)
     (map str)))
 
-
 (defn convert-letter
   [letter-col letter-row]
   (get
     (vec (alphabet-int-from letter-col))
     (letter-position letter-row)))
-
 
 (defn repeat-keyword
   [keyword message]
@@ -34,21 +32,41 @@
     (subs keyword 0 (count message))
     (repeat-keyword (str keyword keyword) message)))
 
+(defn repeat-keyword-vec
+  [keyword message]
+  (->>
+    (repeat-keyword keyword message)
+    (vec)
+    (map str)))
+
+
+(defn join-solution
+  [seq]
+  (->>
+    (vec seq)
+    (clojure.string/join)))
+
+
+(defn decodeLetter
+  [letter-alph letter-idx]
+  (get (vec (alphabet-int-from "a")) (.indexOf (vec (alphabet-int-from (str letter-alph))) letter-idx)))
+
 (defn encode [keyword message]
   "encodeme"
-  (clojure.string/join
-    (vec
-      (let [x (->>
-                (repeat-keyword keyword message)
-                (vec)
-                (map str))]
+  (join-solution
+      (let [x (repeat-keyword-vec keyword message)]
           (map-indexed
-              (fn [idx lt]
-                (convert-letter lt (str (get message idx))))
-              x)))))
+              (fn [idx lt] (convert-letter lt (str (get message idx))))
+              x))))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn decode
+  [keyword message]
+  "decodeme"
+  (join-solution
+      (let [x (repeat-keyword-vec keyword message)]
+        (map-indexed
+          (fn [idx lt] (decodeLetter lt (str (get message idx))))
+          x))))
 
 (defn decipher [cipher message]
   "decypherme")
