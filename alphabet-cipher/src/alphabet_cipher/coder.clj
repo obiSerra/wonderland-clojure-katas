b5768c8634966ff379414cb386629d55b3457642
Roberto <obi.serra@gmail.com>
2016-03-03 23:04:27 +0100

    comments added

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,67 +1,65 @@
 (ns alphabet-cipher.coder)
 
-(defn to-char
-  [letter]
+(defn to-char [letter]
+  "converts a 1 letter string into a char"
   (get (char-array letter) 0))
 
-(defn letter-position
-    [letter]
+(defn letter-position [letter]
+    "returns the letter index inside the a-z alphabet"
     (- (int (to-char letter)) 97))
 
 (defn fill-array [from to start]
-  ""
+  "returns a lazy-seq of integer like (start -> to from -> start)"
   (concat
     (range start (+ to 1))
     (range (+ from 0) start)))
 
 (defn alphabet-int-from [start]
+  "returns a lazy-seq containing the whole alphabet starting from start"
   (->>
     (fill-array (int \a) (int \z) (int (to-char start)))
     (map char)
     (map str)))
 
-(defn convert-letter
-  [letter-col letter-row]
+(defn convert-letter [letter-col letter-row]
+  "converts a letter using the cipher table"
   (get
     (vec (alphabet-int-from letter-col))
     (letter-position letter-row)))
 
-(defn repeat-keyword
-  [keyword message]
+(defn repeat-keyword [keyword message]
+  "repeats keyword until the resulting word is as long as message"
   (if (< (count message) (count keyword))
     (subs keyword 0 (count message))
     (repeat-keyword (str keyword keyword) message)))
 
-(defn add-one-letter
-  [target pool]
+(defn repeat-keyword-vec [keyword message]
+      "returns a vector of string with with repeat-keyword"
+      (->>
+        (repeat-keyword keyword message)
+        (vec)
+        (map str)))
+
+(defn add-one-letter [target pool]
+  "adds a letter from poll to target"
   (clojure.string/join [target (str (get pool (count target)))]))
 
-(defn reduce-keyword
-  [repeated-k keyword]
+(defn reduce-keyword [repeated-k keyword]
+  "returns the keyword used with repeat-keyword"
   (if (= (repeat-keyword keyword repeated-k) repeated-k)
     keyword
     (reduce-keyword
       repeated-k
       (add-one-letter keyword repeated-k))))
 
-
-(defn repeat-keyword-vec
-  [keyword message]
-  (->>
-    (repeat-keyword keyword message)
-    (vec)
-    (map str)))
-
-
-(defn join-solution
-  [seq]
+(defn join-solution [seq]
+  "joins a string concatenating the elements in a lazy-seq"
   (->>
     (vec seq)
     (clojure.string/join)))
 
-
-(defn decode-letter
-  [letter-alph letter-idx]
+(defn decode-letter [letter-alph letter-idx]
+  "decode a letter using the cipher table"
   (get (vec (alphabet-int-from "a")) (.indexOf (vec (alphabet-int-from (str letter-alph))) letter-idx)))
 
 (defn encode [keyword message]
