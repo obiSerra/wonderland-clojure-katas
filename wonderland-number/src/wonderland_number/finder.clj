e697842a649b9d5d84479d2ba037fd4c08e5f80c
Roberto <obi.serra@gmail.com>
2016-03-04 00:00:00 +0100

    all tests passed!!!

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,14 @@
 (ns wonderland-number.finder)
+(defn same-digit?
+  "checks if 2 nums has the same digits"
+  [num-a num-b]
+  (= (sort (vec (str num-a))) (sort (vec (str num-b)))))
 
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (first
+    (filter
+      #(and (same-digit? % (* % 2)) (same-digit? % (* % 3)) (same-digit? % (* % 4)) (same-digit? % (* % 5)) (same-digit? % (* % 6)))
+      (range 99999 999999))))
+
+(wonderland-number)
